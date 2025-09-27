483266e4a4a3cbc3343e2c6d91280c0340efaab4
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2025-09-27 15:44:36 -0300

    Criado a Pagina de Contatos

--- src/Pages/Contatos/index.tsx
+++ src/Pages/Contatos/index.tsx
@@ -1,10 +1,20 @@
 import type { JSX } from "react";
 
+import styles from "./Contatos.module.scss";
+
+import ContatosImg from "../img/contact.svg";
+
 const Contatos = ():JSX.Element => {
     return (
-        <section>
-            <h2>Contatos</h2>
-        </section>
+        <section className={styles.contatos}>
+        <h1>Contatos</h1>
+        <h2>Horário de Funcionamento:</h2>
+        <span>Segunda à Sexta Feira de 09h às 18h</span>
+        <h1 className={styles.telefone}>Telefone:(031)-99999-0000 </h1>
+        <h1 className={styles.email}>e-mail:</h1>
+        <span>agenciatravel@gmail.com</span>
+        <img src={ContatosImg} alt="Imagem de Contatos"/>
+      </section>
     )
 }
 
