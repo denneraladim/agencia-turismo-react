eff55e77b1669d34da994bf95c4f60952adebfb0
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2025-09-27 13:04:41 -0300

    Atualizado

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -5,17 +5,15 @@ import Logo from "../img/logo.svg";
 import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
 
-const Header = ():JSX.Element => {
+const Header = (): JSX.Element => {
     return (
         <header className={styles.header}>
-            <Link to="/">
-                <img src={Logo} alt="Logo" />
-            </Link>
             
+                <img src={Logo} alt="Logo" />
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/sobre">Sobre</Link>
-                <Link to="/contatos">Contatos</Link>
+                <Link to='/'>Home</Link>
+                <Link to='/sobre'>Sobre</Link>
+                <Link to='/contatos'>Contatos</Link>
             </nav>
         </header>
     )
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,25 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Container from "./Components/Container";
+import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
 import Sobre from "./Pages/Sobre";
 import Contatos from "./Pages/Contatos";
-import Footer from "./Components/Footer";
 
-const AppRoutes = () => {
-    return (
-        <BrowserRouter>
-        <Header/>
-        <Container>
-            <Routes>
-                <Route path="/" element={ <Home/>}/>
-                <Route path="/sobre" element={ <Sobre/>}/>
-                <Route path="/contatos" element={ <Contatos/>}/>
-            </Routes>
-            </Container>
-            <Footer/>
-        </BrowserRouter>
-    )
-}
-
-export default AppRoutes;
+<BrowserRouter>
+  <Header />
+  <Container>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/contatos" element={<Contatos />} />
+    </Routes>
+  </Container>
+  <Footer />
+</BrowserRouter>
