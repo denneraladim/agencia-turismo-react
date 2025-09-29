325db1bd1a188e766c1795d99880fd28bbeb9ff5
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2025-09-29 11:56:23 -0300

    Criado a Pagina Not Found

--- src/Pages/NotFound/index.tsx
+++ src/Pages/NotFound/index.tsx
@@ -1,1 +1,19 @@
 
+import styles from "./NotFound.module.scss";
+
+const NotFound = () => {
+    return (
+
+        <section className={styles.container404}>
+            <h2 className={styles.titulo2}>Algo esta  incorreto!</h2>
+            <div className={styles.textos}>
+                <span className={styles.texto_grande}>404</span> <br />
+                <strong className={styles.texto_red}>Página não encontrada!</strong>
+            </div>
+        </section>
+
+    )
+}
+
+
+export default NotFound;
