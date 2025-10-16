9cb7c0232dad87654b7f807b49630ed2b4896c71
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2025-10-16 13:29:39 -0300

    Atualizado

--- src/Pages/Contatos/index.tsx
+++ src/Pages/Contatos/index.tsx
@@ -10,10 +10,10 @@ const Contatos = ():JSX.Element => {
         <h1>Contatos</h1>
         <h2>Horário de Funcionamento:</h2>
         <span>Segunda à Sexta Feira de 09h às 18h</span>
-        <h1 className={styles.telefone}>Telefone:(031)-99999-0000 </h1>
+        <h1 className={styles.telefone}>Telefone:(031)-99999-0000</h1>
         <h1 className={styles.email}>e-mail:</h1>
         <span>agenciatravel@gmail.com</span>
-        <img src={ContatosImg} alt="Imagem de Contatos"/>
+        <img src={ContatosImg}alt="Imagem de Contatos"/>
       </section>
     )
 }
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -7,17 +7,16 @@ import { Link } from "react-router-dom";
 
 const Home = (): JSX.Element => {
     return (
-        <>
-            <section className={styles.home}>
-                <div>
-                    <h1>Bem Vindo <br />
-                        <span>à Nossa Agência</span><br/>
-                        Travel</h1>
-                    <Link to="/sobre" className={`${styles.btn} ${styles.btn_blue}`}>Saiba mais</Link>
-                </div>
-                <img src={HomeImage} alt="Ilustração de avião azul representando viagens"/>
-            </section>
-        </>
+        <section className={styles.home}>
+            <div>
+                <h1>Bem Vindo <br />
+                    <span>à Nossa Agência</span><br />
+                    Travel</h1>
+                <Link to="/sobre" className={`${styles.btn} ${styles.btn_blue}`}>Saiba mais</Link>
+            </div>
+            <img src={HomeImage} alt="Ilustração de avião azul representando viagens" />
+        </section>
+
 
     )
 }
