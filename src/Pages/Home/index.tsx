0d87e5080188a0e1d8599bb1e9fd8e4e2140f867
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2026-01-19 16:24:36 -0300

    Ajustado a Pagina Home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,18 +2,17 @@
 import styles from "./Home.module.scss";
 
 import HomeImage from "../img/airplane-blue.svg";
-import type { JSX } from "react";
 import { Link } from "react-router-dom";
 
-const Home = (): JSX.Element => {
+const Home = () => {
     return (
         <section className={styles.home}>
-            <div>
                 <h1>Bem Vindo <br />
                     <span>à Nossa Agência</span><br />
                     Travel</h1>
-                <Link to="/sobre" className={`${styles.btn} ${styles.btn_blue}`}>Saiba mais</Link>
-            </div>
+                <Link to="/sobre" className={`${styles.btn} ${styles.btn_blue}`}
+                aria-label="Saiba mais sobre a agência"
+                >Saiba mais</Link>
             <img src={HomeImage} alt="Ilustração de avião azul representando viagens" />
         </section>
 
