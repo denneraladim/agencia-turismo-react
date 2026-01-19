1db4e0c5fc027f149652662c680e35ced4f532d1
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2026-01-19 19:24:10 -0300

    Ajuste do Componente Header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,15 +1,13 @@
-import type { JSX } from "react";
-
 import Logo from "../img/logo.svg";
 
 import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
 
-const Header = (): JSX.Element => {
+const Header = () => {
     return (
         <header className={styles.header}>
             
-                <img src={Logo} alt="Logo" />
+                <img src={Logo} alt="Logo da Agência Travel"/>
             <nav>
                 <Link to='/'>Home</Link>
                 <Link to='/sobre'>Sobre</Link>
