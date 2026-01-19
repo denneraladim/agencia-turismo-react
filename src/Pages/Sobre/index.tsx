66b1e03d42219b3f25d393144d69b99cf49e35ad
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2026-01-19 16:56:30 -0300

    Ajustado a Pagina Sobre

--- src/Pages/Sobre/index.tsx
+++ src/Pages/Sobre/index.tsx
@@ -1,24 +1,24 @@
-import type { JSX } from "react";
-
 import styles from "./Sobre.module.scss";
 
 import SobreEmpresa from "../img/undraw_team-work_i1f3.svg";
 
-const Sobre = ():JSX.Element => {
-    return (
-         <section className={styles.sobre}>
-        <div className={styles.content}>
-          <img src={SobreEmpresa} alt="Sobre a empresa"/>
-          <div className={styles.textos}>
-            <h1>Sobre</h1>
-            <p>Somos uma agência de turismo apaixonada por criar experiências
-              inesquecíveis para viajantes de todo o mundo. Com anos de
-              experiência no setor, nossa equipe está empenhada em proporcionar
-              viagens excepcionais, explorando destinos emocionantes e  proporcionando momentos únicos aos nossos clientes.</p>
-          </div>
+const Sobre = () => {
+  return (
+    <section className={styles.sobre}>
+      <article className={styles.content}>
+        <img src={SobreEmpresa} alt="Ilustração representando a equipe da agência de turismo" />
+        <div className={styles.textos}>
+          <h1>Sobre</h1>
+          <p>Somos uma agência de turismo apaixonada por criar experiências
+            inesquecíveis para viajantes de todo o mundo. Com anos de
+            experiência no setor, nossa equipe está empenhada em oferecer
+            viagens excepcionais, explorando destinos emocionantes e
+            criando momentos únicos para nossos clientes.
+          </p>
         </div>
-      </section>
-    )
+      </article>
+    </section>
+  )
 }
 
 export default Sobre;
